4841df7f261fd8974211d5f3097cf12100a84471
sneedlr <lrsjr21@gmail.com>
2017-10-09 18:42:24 -0400

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,55 @@
+#AAAAAAAAAA
+#assign the values to a list
+values=[44, 64, 88, 53, 89]
+#assign the length of the list to a variable
+number=len(values)
+#assign the formula of the average to a variable using the list
+average=sum(values)/number
+#display the results
+print("the average of", number,"given numbers is: ", average)
+#assign the values to a list
+values=[39, 45, 55, 90, 95, 96]
+#assign the length of the list to a variable
+number=len(values)
+#assign the formula of the average to a variable using the list
+average=sum(values)/number
+#display the results
+print("the average of", number,"given numbers is: ", average)
+#assign the values to a list
+values=[54, -45, -10, 90]
+#assign the length of the list to a variable
+number=len(values)
+#assign the formula of the average to a variable using the list
+average=sum(values)/number
+#display the results
+print("the average of", number,"given numbers is: ", average)
+#assign the values to a list
+values=[55, 65, 75, 95, 32]
+#assign the length of the list to a variable
+number=len(values)
+#assign the formula of the average to a variable using the list
+average=sum(values)/number
+#display the results
+print("the average of", number,"given numbers is: ", average)
+#BBBBBB
+#assign variable
+num = 9
+#divide that by two, if the remainder is 0 it is even. display thse results
+if num % 2 == 0:
+    print ("the number", num,"is even.")
+else:
+    print("the number", num,"is odd")
+#assign variable
+num = 19
+#divide that by two, if the remainder is 0 it is even. display thse results
+if num % 2 == 0:
+    print ("the number", num,"is even.")
+else:
+    print("the number", num,"is odd")
+#assign variable
+num = 20
+#divide that by two, if the remainder is 0 it is even. display thse results
+if num % 2 == 0:
+    print ("the number", num,"is even.")
+else:
+    print("the number", num,"is odd")
