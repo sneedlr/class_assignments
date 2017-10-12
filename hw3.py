a926b9aaa77ca82c371f0cf3137434d4126b07d1
sneedlr <lrsjr21@gmail.com>
2017-10-12 09:33:16 -0400

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+script, name, city = argv
+prompt = '> '
+print(f"Hi {name} from the great place of {city} I'm the {script} script.")
+print("As you grow in self-awareness, you remove the blocks to your full potential and naturally live in tune with your true purpose. Meditation and reflection are essential tools on this journey of self-discovery. If you don’t already have a regular meditation practice, we encourage you to begin one. Before you begin each meditation session, ask yourself the four soul questions:")
+print(f"Who are you {name}?")
+who = input(prompt)
+
+print(f"What do you want {name}?")
+want = input(prompt)
+
+print("What are you grateful for?")
+grat = input(prompt)
+
+print(f"""So you said you are {who}. You want {want}. And you are grateful for {grat}. ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- As you continue to ask the questions and spend time each day in the expanded awareness of meditation, the answers will come, perhaps when you’re meditating or perhaps when you’re out in the world going about your everyday activities. Following these steps will expand your ability to follow the trail of your destiny as it unfolds before you moment by moment. ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------""")
